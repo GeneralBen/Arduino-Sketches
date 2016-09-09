2696262f2cc9ee7bfedc400d2c2f06ab4328d0da
ben <benjamin.holmesmith@gmail.com>
2016-09-09 14:08:29 -0700

    Update CarComputer.ino

--- CarComputer.ino
+++ CarComputer.ino
@@ -1,7 +1,7 @@
 /*
- Name:		CarComputer.ino
- Created:	9/9/2016 1:05:35 PM
- Author:	Benjamin
+ Name:    CarComputer.ino
+ Created: 9/9/2016 1:05:35 PM
+ Author:  Benjamin
 */
 
 /****************************************************************************************************************************************************************************
@@ -25,18 +25,78 @@
 #include <LiquidCrystal.h>
 
 // initialize the LCD
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 void setup() 
 {
-	// Start the LCD
-	lcd.begin(16, 1);
+  // Start the LCD
+  lcd.begin(16, 2);
 
-	// Start the serial monitor.  This should be commented out in the final code to conserve memory and increase run speed.
+  // Start the serial monitor.  This should be commented out in the final code to conserve memory and increase run speed.
+  Serial.begin(9600);  
 }
 
 
-void loop() {
-  
+void loop() 
+{
+  testLCD();
+}
+
+void testLCD()
+{
+  int line = 1;
+
+  // test the lcd by writing charicters and clearing them
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = 0; j < 2; j++)
+    {
+      lcd.setCursor(0, j);
+      for (int k = 1; k <= 16; k++)
+      {
+        lcd.print(i);
+      }
+    }
+
+    delay(500);
+    
+    lcdLineClear(line++);
+
+    if (line == 4)
+    {
+      line = 1;
+    }
+
+    delay(500);
+
+  }
+}
+
+// This function clears the lcd when called
+void lcdLineClear(int line)
+{
+  switch (line)
+  {
+
+  // clear the first line
+  case 1:
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    break;
+
+  // clear second line
+  case 2:
+    lcd.setCursor(0, 1);
+    lcd.print("                ");
+    break;
+
+  //clear both lines by calling self
+  case 3:
+    lcdLineClear(1);
+    lcdLineClear(2);
+    break;
+
+  default:
+    break;
+  }
 }
